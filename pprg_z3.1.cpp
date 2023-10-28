54e56fc7c31a5c42e02149b86d4e14a25f8db0d8
Bartosz Waszczyk <s32351@pjwstk.edu.pl>
2023-10-28 12:56:41 +0200

    exercise 3, vscode workspace

--- pprg_z3.1.cpp
+++ pprg_z3.1.cpp
@@ -10,10 +10,11 @@ int main()
     cin >> numY;
     cout << "podałeś liczbę X = " << numX << " oraz liczbę Y = " << numY << "\n";
     cout << "wyniki: \n" ;
-    
+
     if (numX == 0) {
        cout << "druga liczba to 0, instrukcja wyik dzielenia w zależności o interpretacji nie istnieje lub jest nieskończonością. \n";
         } else cout << "iloraz liczb: " << printf("%.2f\n",numX/numY);
+      
        cout << "suma liczb: " << printf("%.2f\n",numX+numY) ;
        cout << "różnica liczb: " << printf("%.2f\n",numX-numY);
        cout << "iloczyn liczb: " << printf("%.2f\n",numX*numY);
